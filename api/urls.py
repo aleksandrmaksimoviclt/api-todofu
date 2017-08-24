90cb17e15e432c62bbabee8bc6f77aa98e9ec07f
Aleksandr Maksimovic <aleksandr.maksimoviclt@gmail.com>
2017-08-24 23:24:50 +0200

    Add Django Admin feature

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,13 @@ api URL Configuration
 """
 
 from django.conf.urls import url, include
-
+from django.contrib import admin
 
 urlpatterns = [
+
+    url(r'^admin/', admin.site.urls),
+
+    # API
     url(r'^v1/', include('v1.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
